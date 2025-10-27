b65382b5f175e6d1acddc903808b1c928953ea60
Carmelo Sammarco <sammarcocarmelo@gmail.com>
2025-10-27 13:46:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WEBHOOK_URL = 'https://racheal-postasthmatic-irrigably.ngrok-free.dev/webhook/41b75593-b1bd-4234-969d-51c36e29a6fd';
+const WEBHOOK_URL = 'https://racheal-postasthmatic-irrigably.ngrok-free.dev/webhook-test/ffb578b0-75d8-4631-ac6a-f43b93bb7501';
 
 let chatMessages, messageInput, sendButton;
 
